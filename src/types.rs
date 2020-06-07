de1700c74d1da3bf63c2e30c88af1d34f75274da
Damian Poddebniak <poddebniak@fh-muenster.de>
2020-06-07 13:47:23 +0200

    added pub Command::serialize

--- src/types.rs
+++ src/types.rs
@@ -87,4 +87,69 @@ impl Command {
             Command::Auth => "AUTH",
         }
     }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        match self {
+            Command::User(user) => format!("USER {}\r\n", user).into_bytes(),
+            Command::Pass(pass) => format!("PASS {}\r\n", pass).into_bytes(),
+            Command::Stat => b"STAT\r\n".to_vec(),
+            Command::List { msg } => match msg {
+                None => b"LIST\r\n".to_vec(),
+                Some(msg) => format!("LIST {}\r\n", msg).into_bytes(),
+            },
+            Command::Retr { msg } => format!("RETR {}\r\n", msg).into_bytes(),
+            Command::Dele { msg } => format!("DELE {}\r\n", msg).into_bytes(),
+            Command::Noop => b"NOOP\r\n".to_vec(),
+            Command::Rset => b"RSET\r\n".to_vec(),
+            Command::Quit => b"QUIT\r\n".to_vec(),
+            Command::Apop { name, digest } => format!("APOP {} {}\r\n", name, digest).into_bytes(),
+            Command::Top { msg, n } => format!("TOP {} {}\r\n", msg, n).into_bytes(),
+            Command::Uidl { msg } => match msg {
+                None => b"UIDL\r\n".to_vec(),
+                Some(msg) => format!("UIDL {}\r\n", msg).into_bytes(),
+            },
+            Command::Capa => b"CAPA\r\n".to_vec(),
+            Command::Stls => b"STLS\r\n".to_vec(),
+
+            Command::Auth => b"AUTH\r\n".to_vec(),
+            Command::AuthPlain => unimplemented!(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Command;
+
+    #[test]
+    fn test_serialize() {
+        assert_eq!(Command::User("alice".into()).serialize(), b"USER alice\r\n");
+        assert_eq!(
+            Command::Pass("password".into()).serialize(),
+            b"PASS password\r\n"
+        );
+        assert_eq!(Command::Stat.serialize(), b"STAT\r\n");
+        assert_eq!(Command::List { msg: None }.serialize(), b"LIST\r\n");
+        assert_eq!(Command::List { msg: Some(1) }.serialize(), b"LIST 1\r\n");
+        assert_eq!(Command::Retr { msg: 1 }.serialize(), b"RETR 1\r\n");
+        assert_eq!(Command::Dele { msg: 1 }.serialize(), b"DELE 1\r\n");
+        assert_eq!(Command::Noop.serialize(), b"NOOP\r\n");
+        assert_eq!(Command::Rset.serialize(), b"RSET\r\n");
+        assert_eq!(Command::Quit.serialize(), b"QUIT\r\n");
+        assert_eq!(
+            Command::Apop {
+                name: "alice".into(),
+                digest: "aabbccddeeff".into()
+            }
+            .serialize(),
+            b"APOP alice aabbccddeeff\r\n"
+        );
+        assert_eq!(Command::Top { msg: 1, n: 5 }.serialize(), b"TOP 1 5\r\n");
+        assert_eq!(Command::Uidl { msg: None }.serialize(), b"UIDL\r\n");
+        assert_eq!(Command::Uidl { msg: Some(1) }.serialize(), b"UIDL 1\r\n");
+        assert_eq!(Command::Capa.serialize(), b"CAPA\r\n");
+        assert_eq!(Command::Stls.serialize(), b"STLS\r\n");
+        assert_eq!(Command::Auth.serialize(), b"AUTH\r\n");
+        //assert_eq!(Command::AuthPlain.serialize(), b"\r\n");
+    }
 }
